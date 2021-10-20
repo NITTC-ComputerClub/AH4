843db1e80916d4f74ad2ba5a86b095f1e37f900f
gb72127 <takutora589@outlook.jp>
2021-10-20 17:45:03 +0900

    物体が落ちます

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 import sys
 from pygame.locals import *
 
@@ -7,16 +8,39 @@ def main():
     
     screen=pygame.display.set_mode((500,400))
     
+    ck = pygame.time.Clock()
+
+
     y=40
     x=210
+
+    pos = [(20, 400), (100, 150), (40, 200), (0, 10)]
+    
+    cx = random.randint(100,300)
+    cy = random.randint(0,100)
+
+    flag = False
+
     while(1):
-        screen.fill((255,120,155))
+        ck.tick(60)
+        
+        if(flag):
+            cx = random.randint(100,300)
+            cy = random.randint(0,100)
+            flag = False
+
+        screen.fill((255,255,255))
         
         pygame.draw.rect(screen,(40,40,255),Rect(x,200,100,100))
-        pygame.draw.rect(screen,(250,150,30),Rect(235,y,30,30))
-        pygame.draw.rect(screen,(200,200,100),Rect(200,y,20,50))
+        pygame.draw.rect(screen,(250,150,30),Rect(cx,cy,30,30))
+        #pygame.draw.rect(screen,(200,200,100),Rect(200,y,20,50))
+
+        for p in pos:
+            pygame.draw.rect(screen,(250,150,30),Rect(p[0],p[1],30,30))
         
-        x=x+1
+        for i in range(0,len(pos)):
+            pos[i] = (pos[i][0],pos[i][1] + 1)
+        
         
         if x>=380:
             x=10
@@ -30,6 +54,12 @@ def main():
             if(event.type==QUIT):
                 pygame.quit()
                 sys.exit()
+            if(event.type == MOUSEMOTION):
+                x,y = event.pos
+                x = x - 50
+            if(event.type == MOUSEBUTTONDOWN):
+                flag = True
+
         
         pygame.display.update()
 
